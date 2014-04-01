1e23f960911ff993ebe9102540089cc1aa0783df
Tim Wood <timwood@gwu.edu>
2014-04-01 13:16:01 -0400

    fixed time measurements using rdtsc

--- memory/conctests.c
+++ memory/conctests.c
@@ -9,66 +9,79 @@
 
 volatile unsigned long long sharedInt=0;
 
-#define LOOPS 100000000
+#define LOOPS 10000000
 
 
-static __inline__ unsigned long long
+static __attribute__((always_inline)) unsigned long long
 rdtsc (void)
 {
   unsigned long long int x;
   __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
   return x;
 }
-static inline void native_cpuid(unsigned int *eax, unsigned int *ebx,
+static __attribute__((always_inline)) void
+native_cpuid(unsigned int *eax, unsigned int *ebx,
                                 unsigned int *ecx, unsigned int *edx)
 {
-        /* ecx is often an input as well as an output. */
-        asm volatile("cpuid"
-            : "=a" (*eax),
-              "=b" (*ebx),
-              "=c" (*ecx),
-              "=d" (*edx)
-            : "0" (*eax), "2" (*ecx));
+  /* ecx is often an input as well as an output. */
+  __asm__ volatile("cpuid"
+      : "=a" (*eax),
+        "=b" (*ebx),
+        "=c" (*ecx),
+        "=d" (*edx)
+      : "0" (*eax), "2" (*ecx));
+}
+
+static __attribute__((always_inline)) unsigned long long
+syncrdtsc (void)
+{
+  unsigned int eax, ebx, ecx, edx;
+  eax = 1;
+  unsigned long long int t;
+  //native_cpuid(&eax, &ebx, &ecx, &edx);
+  t = rdtsc();
+  return t;
 }
 
 unsigned long long standardIncLoop(){
   int i;
-  unsigned long long start;
+  unsigned long long timer;
 
-  unsigned eax, ebx, ecx, edx;
+  sharedInt = 0;
 
-  eax = 1; /* processor info and feature bits */
-  native_cpuid(&eax, &ebx, &ecx, &edx);
-  start = rdtsc();
+  timer = syncrdtsc();
 
   for(i=0; i < LOOPS; i++){
     sharedInt++;
   }
-  native_cpuid(&eax, &ebx, &ecx, &edx);
-  start =  rdtsc() - start;
+  timer =  syncrdtsc() - timer;
   printf("Finished %llu loops\n", sharedInt);
-  return start;
+  return timer;
 }
 
 unsigned long long atomicIncLoop(){
   int i;
-  unsigned long long start;
+  unsigned long long timer;
 
-  unsigned eax, ebx, ecx, edx;
+  sharedInt = 0;
 
-  eax = 1; /* processor info and feature bits */
-  native_cpuid(&eax, &ebx, &ecx, &edx);
-  start = rdtsc();
+  timer = syncrdtsc();
 
   for(i=0; i < LOOPS; i++){
     g_atomic_int_inc(&sharedInt);
   }
-  sharedInt++;
-  native_cpuid(&eax, &ebx, &ecx, &edx);
-  sharedInt--;
-  start =  rdtsc() - start;
+  timer =  syncrdtsc() - timer;
   printf("Finished %llu loops\n", sharedInt);
-  return start;
+  return timer;
+}
+
+unsigned long long timedsleep(int millisleep) {
+  unsigned long long timer;
+  timer = syncrdtsc();
+  usleep(millisleep*1000);
+  timer =  syncrdtsc() - timer;
+  return timer;
+
 }
 
 int main()
@@ -76,19 +89,24 @@ int main()
   unsigned long long time;
   unsigned long long timeFunc;
 
+  // Intel doc says to run CPUID > 3 times at timer since it may be slow
+  // the first few times you run it.
+  printf("Trial runs (not timed)...\n");
+  standardIncLoop();
+  standardIncLoop();
+  standardIncLoop();
+
+  printf("Time to sleep 1 second...\n");
+  time = timedsleep(1000);
+  printf("Cycles for 1000 msec sleep: %llu\n", time);
+
   printf("Running standard increment loop %d times\n", LOOPS);
-  timeFunc = rdtsc();
   time = standardIncLoop();
-  timeFunc = rdtsc() - timeFunc;
-  printf("Cycles per iteration: %f  cycles for function: %f\n", ((float)time)/LOOPS, ((float)timeFunc)/LOOPS);
-  printf("Cycles: %llu  cycles for function: %llu\n", time, timeFunc);
+  printf("Cycles per iteration: %f\n", ((float)time)/LOOPS);
 
   printf("Running atomic increment loop %d times\n", LOOPS);
-  timeFunc = rdtsc();
   time = atomicIncLoop();
-  timeFunc = rdtsc() - timeFunc;
-  printf("Cycles per iteration: %f  cycles for function: %f\n", ((float)time)/LOOPS, ((float)timeFunc)/LOOPS);
-  printf("Cycles: %llu  cycles for function: %llu\n", time, timeFunc);
+  printf("Cycles per iteration: %f\n", ((float)time)/LOOPS);
 
 
 }
